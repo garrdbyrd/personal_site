51ac3117eb717ed630d61314fb2c430f584e5bb2
Garrett Byrd <gbyrd4@vols.utk.edu>
2023-04-04 06:45:41 -0500

    nojekyll to main

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,20 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static';
 
-export default {
-  //preprocess: vitePreprocess(),
+const config = {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
-    })
+      // pages: 'build',
+      // assets: 'build',
+      // fallback: null,
+      // precompress: false,
+      // strict: true
+    }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+    },
   }
-}
+};
+
+export default config
