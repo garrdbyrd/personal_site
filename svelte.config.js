cfb41c00d17e13267d556a434e8dabb46f902ee5
Garrett Byrd <gbyrd4@vols.utk.edu>
2023-04-04 07:03:31 -0500

    added @type line to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static';
 
 const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
